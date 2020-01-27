271c6613f94ad899579b87b4938e2912031cbd2f
tomascristian <tomascristian@gmail.com>
2020-01-27 08:55:47 -0300

    Initial version

---
+++ README.md
@@ -0,0 +1,21 @@
+# bookfinder
+https://tomascristian.github.io/bookfinder/
+
+## Overview
+
+Proof-of-concept App following [Chingu Voyage Pre-Work Tier 3 Solo Project Specifications](https://github.com/chingu-voyages/voyage-prework-tier3-bookfinder).
+
+It queries the [Google Books API](https://developers.google.com/books/) and displays a list of 10 books matching the user's search terms.
+
+## Highlights
+
+- Minimalistic code. Just one HTML file, a CSS file and a JavaScript file (about 200 lines all three put together).
+- No dependencies or build process.
+- Directly runnable in the browser.
+- No media queries. All responsive goodness courtesy of CSS Grid.
+
+## Other notes
+
+- Internet Explorer is not supported.
+- No tests, sorry! :(
+- It may have some minor styling issues (particularly whith the form elements, due to inconsistent vendor styles on different browsers).
---
+++ script.js
@@ -0,0 +1,66 @@
+"use strict";
+
+let lastSearchTerms = "";
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  const searchTerms = event.target.search.value;
+  if (searchTerms === lastSearchTerms) return;
+  lastSearchTerms = searchTerms;
+  performSearch(searchTerms);
+});
+
+function performSearch(searchTerms) {
+  const main = document.querySelector("main");
+  main.innerHTML = "LOADING...";
+  fetchBooks(searchTerms)
+    .then((results) => {
+      if (results.totalItems === 0) {
+        main.innerHTML = "There were no results for your search terms.";
+        return;
+      }
+      const bookCards = results.items.map(processBookInfo).map(buildBookCard);
+      main.innerHTML = bookCards.join("\n");
+    })
+    .catch((error) => {
+      main.innerHTML = "There was an error. Please try again later.";
+      console.error(error);
+    });
+}
+
+function fetchBooks(searchTerms) {
+  const searchQuery = "q=" + encodeURIComponent(searchTerms);
+  const fields = "&fields=totalItems,items(volumeInfo/imageLinks,volumeInfo/title,volumeInfo/authors,volumeInfo/publisher,volumeInfo/infoLink)";
+  const queryString = searchQuery + fields;
+  const url = "https://www.googleapis.com/books/v1/volumes?" + queryString;
+
+  const results = fetch(url).then((response) => {
+    const { ok, status, statusText } = response;
+    if (!ok) throw Error(`Response returned: ${status} ${statusText}`);
+    return response.json();
+  });
+
+  return results;
+}
+
+function processBookInfo(item) {
+  const { imageLinks, title, authors, publisher, infoLink } = item.volumeInfo;
+  return {
+    cover: imageLinks ? imageLinks.thumbnail : "",
+    title,
+    author: authors ? authors.join(", ") : "Unknown",
+    publisher: publisher || "Unknown",
+    link: infoLink
+  };
+}
+
+function buildBookCard(book) {
+  const { cover, title, author, publisher, link } = book;
+  return (
+    `<div class="book-card" >
+      <img src=${cover} alt="book cover" width="128"/>
+      <h2>${title}</h2>
+      <div>By: ${author}</div>
+      <div>Publisher: ${publisher}</div>
+      <a href=${link}>See this book</a>
+    </div>`);
+}
